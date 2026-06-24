bac1668a385946826a2b4e6d1a476132122f43db
Dawid Staszewski <dawid.staszewski.ext@zeiss.com>
2026-06-24 11:22:43 +0200

    Added: Integration tests to CashedProductRepositoryTests

--- back-product-mgmt/Infrastructure.Tests/Data/CashedProductRepositoryTests.cs
+++ back-product-mgmt/Infrastructure.Tests/Data/CashedProductRepositoryTests.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+using Core.Entities;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.Extensions.Caching.Distributed;
@@ -6,6 +7,7 @@ using Microsoft.Extensions.Caching.StackExchangeRedis;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using NSubstitute;
+using NSubstitute.ReceivedExtensions;
 using Testcontainers.Redis;
 
 namespace Infrastructure.Tests.Data;
@@ -47,4 +49,138 @@ public class CashedProductRepositoryTests : IAsyncLifetime
         _realRedisCache = new RedisCache(options);
         _sut = new CashedProductRepository(_innerRepositoryMock, _realRedisCache);
     }
+
+    [Fact]
+    public async Task GetByIdAsync_WhenUuidIsFoundInCache_ReturnProduct()
+    {
+        //Arrange
+        var uuid = Guid.NewGuid();
+        string cacheKey = $"product:{uuid}";
+        var testProduct = new Product(uuid, "Test", ProductCategory.GraphicsCards, 10.99m, 1);
+        var testProductJson = JsonSerializer.Serialize(testProduct, _jsonOptions);
+        await _realRedisCache.SetStringAsync(cacheKey, testProductJson);
+        //Act
+        var result = await _sut.GetByIdAsync(uuid);
+        //Assert
+        Assert.NotNull(result);
+        Assert.Equal(uuid, result.Uuid);
+        Assert.Equal(testProduct.Name, result.Name);
+        Assert.Equal(testProduct.CategoryId, result.CategoryId);
+        Assert.Equal(testProduct.Quantity, result.Quantity);
+        Assert.Equal(testProduct.Price, result.Price);
+        await _innerRepositoryMock.DidNotReceive().GetByIdAsync(uuid);
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_WhenUuidIsNotFoundInCache_ReturnProduct()
+    {
+        //Arrange
+        var uuid = Guid.NewGuid();
+        string cacheKey = $"product:{uuid}";
+        var testProduct = new Product(uuid, "Test", ProductCategory.GraphicsCards, 10.99m, 1);
+        _innerRepositoryMock.GetByIdAsync(uuid).Returns<Product?>(testProduct);
+        //Act
+        var result = await _sut.GetByIdAsync(uuid);
+        //Assert
+        Assert.NotNull(result);
+        Assert.Equal(uuid, result.Uuid);
+        Assert.Equal(testProduct.Name, result.Name);
+        Assert.Equal(testProduct.CategoryId, result.CategoryId);
+        Assert.Equal(testProduct.Quantity, result.Quantity);
+        Assert.Equal(testProduct.Price, result.Price);
+        await _innerRepositoryMock.Received(1).GetByIdAsync(uuid);
+        var cacheProductJson = await _realRedisCache.GetStringAsync(cacheKey);
+        var cacheProduct = JsonSerializer.Deserialize<Product?>(cacheProductJson!, _jsonOptions);
+        Assert.NotNull(cacheProduct);
+        Assert.Equal(uuid, cacheProduct.Uuid);
+        Assert.Equal(testProduct.Name, cacheProduct.Name);
+        Assert.Equal(testProduct.CategoryId, cacheProduct.CategoryId);
+        Assert.Equal(testProduct.Quantity, cacheProduct.Quantity);
+        Assert.Equal(testProduct.Price, cacheProduct.Price);
+    }
+    [Fact]
+    public async Task GetByIdAsync_WhenUuidIsNotFound_ReturnNull()
+    {
+        //Arrange
+        var uuid = Guid.NewGuid();
+        string cacheKey = $"product:{uuid}";
+        _innerRepositoryMock.GetByIdAsync(uuid).Returns<Product?>((Product?)null);
+        //Act
+        var result = await _sut.GetByIdAsync(uuid);
+        //Assert
+        Assert.Null(result);
+        await _innerRepositoryMock.Received(1).GetByIdAsync(uuid);
+        var cacheProductJson = await _realRedisCache.GetStringAsync(cacheKey);
+        Assert.Null(cacheProductJson);
+    }
+    [Fact]
+    public async Task UpdateAsync_WhenProductIsExist_UpdatesDatabaseAndSavesToCache()
+    {
+        //Arrange
+        var uuid = Guid.NewGuid();
+        string cacheKey = $"product:{uuid}";
+        var testProduct = new Product(uuid, "Test", ProductCategory.GraphicsCards, 10.99m, 1);
+        _innerRepositoryMock.UpdateAsync(uuid, testProduct).Returns<Product?>(testProduct);
+        //Act
+        var result = await _sut.UpdateAsync(uuid, testProduct);
+        //Assert
+        Assert.NotNull(result);
+        Assert.Equal(testProduct.Name, result.Name);
+        Assert.Equal(testProduct.CategoryId, result.CategoryId);
+        Assert.Equal(testProduct.Quantity, result.Quantity);
+        Assert.Equal(testProduct.Price, result.Price);
+        await _innerRepositoryMock.Received(1).UpdateAsync(uuid, testProduct);
+        var cacheProductJson = await _realRedisCache.GetStringAsync(cacheKey);
+        var cacheProduct = JsonSerializer.Deserialize<Product?>(cacheProductJson!, _jsonOptions);
+        Assert.NotNull(cacheProduct);
+        Assert.Equal(uuid, cacheProduct.Uuid);
+        Assert.Equal(testProduct.Name, cacheProduct.Name);
+        Assert.Equal(testProduct.CategoryId, cacheProduct.CategoryId);
+        Assert.Equal(testProduct.Quantity, cacheProduct.Quantity);
+        Assert.Equal(testProduct.Price, cacheProduct.Price);
+    }
+    [Fact]
+    public async Task UpdateAsync_WhenProductIsNotExist_NotUpdatedInCache()
+    {
+        //Arrange
+        var uuid = Guid.NewGuid();
+        string cacheKey = $"product:{uuid}";
+        var testProduct = new Product(uuid, "Test", ProductCategory.GraphicsCards, 10.99m, 1);
+        _innerRepositoryMock.UpdateAsync(uuid, testProduct).Returns<Product?>((Product?)null);
+        //Act
+        var result = await _sut.UpdateAsync(uuid, testProduct);
+        //Assert
+        Assert.Null(result);
+        await _innerRepositoryMock.Received(1).UpdateAsync(uuid, testProduct);
+        var cacheProductJson = await _realRedisCache.GetStringAsync(cacheKey);
+        Assert.Null(cacheProductJson);
+    }
+    [Fact]
+    public async Task DeleteAsync_WhenUuidIsValid_DeletedProduct()
+    {
+        var uuid = Guid.NewGuid();
+        string cacheKey = $"product:{uuid}";
+        var testProduct = new Product(uuid, "Test", ProductCategory.GraphicsCards, 10.99m, 1);
+        await _realRedisCache.SetStringAsync(cacheKey, JsonSerializer.Serialize<Product>(testProduct, _jsonOptions));
+        _innerRepositoryMock.DeleteAsync(uuid).Returns(true);
+        //Act
+        var result = await _sut.DeleteAsync(uuid);
+        //Assert
+        Assert.True(result);
+        await _innerRepositoryMock.Received(1).DeleteAsync(uuid);
+        var cacheResult = await _realRedisCache.GetStringAsync(cacheKey);
+        Assert.Null(cacheResult);
+    }
+    /*
+    public async Task<bool> DeleteAsync(Guid uuid)
+    {
+        var isDeleted = await _innerRepository.DeleteAsync(uuid);
+
+        if (isDeleted)
+        {
+            await _cache.RemoveAsync($"product:{uuid}");
+        }
+        return isDeleted;
+    }
+    }*/
 }
